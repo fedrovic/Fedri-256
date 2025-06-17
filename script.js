857f2a95ff3c37b2dade0479ccc05463b5aa2c51
fedrovic <fredakunzire@gmail.com>
2025-06-18 01:59:05 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// DARK MODE TOGGLE
+const toggleBtn = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+toggleBtn.addEventListener('click', () => {
+  body.classList.toggle('dark');
+  // Change icon
+  const icon = toggleBtn.querySelector('i');
+  if(body.classList.contains('dark')){
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+  } else {
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
+  }
+});
+
+// RESPONSIVE NAVBAR TOGGLE
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEvent
